a7d27cdd5689abb57fb1581b99cd30dd80be6910
wejanis <will.janis@gmail.com>
2015-09-03 20:33:14 -0700

    [addUser.php]: Removed a debug echo statement. Removed else part of branch and made failure the default response. Now always returns a response regardless of if statement.

--- addUser.php
+++ addUser.php
@@ -1,36 +1,41 @@
 <?php 
+
+//Using RedBean object relational mapper to query the database.
 require 'rb.php';
 
+//array for json response
 $response = array();
+$response["success"] = 0;
+$response["message"] = "A required field is missing!";
+//Connect to database using redbean
+R::setup('mysql:host=localhost;dbname=firstdb','root', '33Xddy2fNWDW5NQG' );
 
 if(isset($_POST['name']) && isset($_POST['username']) && isset($_POST['password']) && isset($_POST['verifypassword']))
 {
-	$id = -1;
-	R::setup( 'mysql:host=localhost;dbname=firstdb','root', '33Xddy2fNWDW5NQG' );
+	$id = -1;	
+
+	//Create a user "bean" with information from the post request and store it in the database
 	$user = R::dispense('users');
 	$user->name = $_POST['name'];
 	$user->username = $_POST['username'];
 	$user->password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-	$id = R::store($user);
+	$id = R::store($user); //store returns the id
 	
+	//If the id has changed, the bean was successfully stored.
 	if($id != -1)
 	{
 		$response["success"] = 1;
 		$response["message"] = "Account successfully created.";
-		echo json_encode($response);
 	}
+	//Otherwise, the bean failed to store
 	else{
 		$response["success"] = 0;
 		$response["message"] = "Failed to create account.";
-		echo json_encode($repsonse);
 	}
-	echo("Created new user: " . $user->username);
+	//echo("\nCreated new user: " . $user->username);
 	R::close();
 }
 
-else{
-	$response["success"] = 0;
-	$response["message"] = "A required field is missing!";
-	echo json_encode($response);
-}
+echo json_encode($response);
+die();
 ?>
