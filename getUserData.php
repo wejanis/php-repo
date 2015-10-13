2dc1764c7d4958b56bee0660d1b37833f7e32807
wejanis <will.janis@gmail.com>
2015-10-12 23:56:52 -0700

    getuserdata.php queries the database to find all exercise information for a user on the given day and returns it as a json array.

--- getUserData.php
+++ getUserData.php
@@ -8,11 +8,23 @@ $response = array();
 
 //Connect to database using redbean
 R::setup('mysql:host=localhost;dbname=firstdb','root', '33Xddy2fNWDW5NQG' );
-R::debug(true);
+//R::debug(true);
 
 $username = $_GET['username'];
+$date = $_GET['exercise_date'];
 
+$username_result = R::findOne('user', 'username = ?', array($username));
 
+$user_id = $username_result->getId();
+
+$exercises = R::getAll( 
+	'select exercise_name, weight, reps, exercise_date, exercise_complete 
+	 from exercises 
+	 where user_id = :id AND exercise_date = :date', 
+	 array(':id' => $user_id,':date' => $date)
+	);
+
+echo json_encode($exercises);
 
 
 
